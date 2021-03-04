3dd04bb806c041c1ddb57bdc8410bee59c578696
Bhavya Bibi <bhavyabibi007@gmail.com>
2021-03-04 07:36:36 +0400

    home page design completed

---
+++ wedding/wedding/appz/core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.conf import settings
+
+# Create your views here.
+
+
+
+
+def home(request):
+
+    
+
+
+
+    return render(request,'core/home.html')
